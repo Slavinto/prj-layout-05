2e3dde40bbaf29a641fc242ef23aaacf975077ec
slavinto <slava3669@gmail.com>
2023-03-22 17:15:04 +0300

    Home section layout

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,17 @@ import * as flsFunctions from "./modules/functions.js";
 
 flsFunctions.isWebp();
 
-import Swiper, { Navigation, Pagination } from "swiper";
+// import Swiper, { Navigation, Pagination } from "swiper";
 
-const swiper = new Swiper();
+// const swiper = new Swiper();
+const menu = document.querySelector(".menu__icon");
+const menuBody = document.querySelector(".menu__body");
+const menuChildren = document.querySelector(".menu__icon").children;
+
+menu.addEventListener("click", () => {
+    menu.classList.toggle("_active");
+    menuBody.classList.toggle("_active");
+    // Array.from(menuChildren).forEach((child) =>
+    //     child.classList.toggle("_active")
+    // );
+});
