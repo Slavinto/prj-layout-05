f386d156e3d1233a5d42590d588a82e20945bffe
slavinto <slava3669@gmail.com>
2023-08-03 17:04:45 +0300

    initial commit

--- src/js/app.js
+++ src/js/app.js
@@ -4,9 +4,10 @@ flsFunctions.isWebp();
 
 const menu = document.querySelector(".menu__icon");
 const menuBody = document.querySelector(".menu__body");
+const menuList = document.querySelector(".menu__list");
 
 menu.addEventListener("click", () => {
     menu.classList.toggle("_active");
     menuBody.classList.toggle("_active");
-
+    menuList.classList.toggle("_active");
 });
