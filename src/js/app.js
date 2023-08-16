4984223a4d6cdf65bd970a6a60d5a571638b9cc0
slavinto <slava3669@gmail.com>
2023-08-16 17:01:06 +0300

    v.1.4.1 basic services section layout - no adaptive

--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,7 @@ const menu = document.querySelector(".menu__icon");
 const menuBody = document.querySelector(".menu__body");
 const menuList = document.querySelector(".menu__list");
 
+
 menu.addEventListener("click", () => {
     menu.classList.toggle("_active");
     menuBody.classList.toggle("_active");
